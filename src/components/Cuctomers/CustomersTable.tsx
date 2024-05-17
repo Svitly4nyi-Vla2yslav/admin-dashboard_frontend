58ba3083c0a67893c07f944995317dc903ea1bf4
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-05-17 22:34:58 +0200

    add new components

---
+++ src/components/Cuctomers/CustomersTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomersTable: React.FC = () => {
+  return (
+    <div>CustomersTable</div>
+  )
+}
+
+export default CustomersTable
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Header: React.FC = () => {
+
+  return (
+    <div>
+   Header
+    </div>
+  );
+};
